11edf780f80336653960728fa82b38fee4d1ef40
Edward Delaporte <edthedev@gmail.com>
2015-01-31 19:07:05 -0600

    Added suggested bits from website.

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,17 @@
+# hugo
+
+# Add changes to git.
 git checkout master
+git add -A
+
+# Commit changes.
+msg="rebuilding site `date`"
+if [ $# -eq 1 ]
+	then msg="$1"
+fi
+git commit -m "$msg"
+
+# Push source and build repos.
+git push origin master
+
 git subtree push --prefix=public https://github.com/edthedev/edward.delaporte.us.git gh-pages
